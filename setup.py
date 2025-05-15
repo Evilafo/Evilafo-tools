1454c11ef140928eda9cd6b2c554ae218540b05f
Evilafo Emmanuel <36644643+Evilafo@users.noreply.github.com>
2025-05-15 19:06:57 +0000

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Evilafo-tools',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        'transformers',  
+        'torch', 
+        'numpy',  
+    ],
+    description='A Python library for using BERT and BART',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Emmanuel Evilafo',
+    author_email='evil2846@gmail.com',
+    url='https://github.com/Evilafo/Evilafo-tools',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
