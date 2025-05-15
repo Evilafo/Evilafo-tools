88f9c537faae2aa838be1e15cd7e9840192da0de
Evilafo Emmanuel <36644643+Evilafo@users.noreply.github.com>
2025-05-15 21:06:55 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='Evilafo-tools',
+    name='evilafo-tools',
     version='0.1',
     packages=find_packages(),
     install_requires=[
